8876bba64230b80aa5ab3b1c215222e2f2c8cbef
Rifa Achrinza <25147899+achrinza@users.noreply.github.com>
2024-04-28 19:26:03 +0800

    feat: complete home screen
    Signed-off-by: Rifa Achrinza <25147899+achrinza@users.noreply.github.com>

---
+++ packages/frontend/src/main/screen/explore.cljs
@@ -0,0 +1,27 @@
+(ns screen.explore
+  (:require [reagent.core :as r]
+            ["react-native" :as rn]))
+
+(defn explore-screen [{navigation :navigation route :route}]
+    (let [category (js->clj (.-category (.-params route)))
+          categories {:self {:name "About Self"}}
+          articles [{:title "Know Your Personality"
+                     :category :self
+                     :sections [{:title "Lorem Ipsum"
+                                 :body "Sit Amet."}]}]]
+      [:> rn/ScrollView
+       [:> rn/Text
+        (categories (keyword category))]]
+
+      (->> articles
+           (filter (fn [{article-category :category}]
+                     (= article-category category)))
+           (map (fn [{title :title :as article}]
+                  [:> rn/Pressable {:style {:background "#FFF"
+                                            :borderRadius "12px"
+                                            :marginBottom "10px"}
+                                    :onPress #(navigation.navigate "Article" (clj->js {:article article}))}
+                   [:> rn/Text title]]))
+           (into [:> rn/View
+                  [:> rn/Text {:style {:fontSize "24px"}}
+                   "Articles"]]))))
