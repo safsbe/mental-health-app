a19d62f5a770d6b833206979eb14f57ba28a0f89
Kimi Yap <ephie8654@outlook.com>
2024-04-29 23:55:16 +0800

    feat(article): added 2 articles
    Signed-off-by: Kimi Yap <ephie8654@outlook.com>

--- packages/frontend/src/main/screen/article.cljs
+++ packages/frontend/src/main/screen/article.cljs
@@ -2,15 +2,21 @@
   (:require [reagent.core :as r]
             ["react-native" :as rn]))
 
-(defn article-screen [{route :route}]
+(defn article-screen [{navigation :navigation route :route}]
   (let [article (:article (.-params route))
         title (:title article)
         sections (:sections article)]
+    (navigation.setOptions #js{:title title})
+    ;(js/console.log (str title))
     (->> sections
          (map (fn [{title :title body :body}]
-                [:> rn/View
-                 [:> rn/Text {:style {:fontSize "24px"
-                                      :marginTop "10px"}}
+                [:> rn/View {:style {:marginLeft :20px
+                                     :marginRight :20px}}
+                 [:> rn/Text {:style {:fontSize "16px"
+                                      :marginTop "10px"
+                                      :fontWeight :bold
+                                      :textDecorationLine :underline}}
                   title]
-                 [:> rn/Text body]]))
+                 [:> rn/Text {:style {:fontSize :14px}} 
+                  body]]))
          (into [:> rn/ScrollView]))))
